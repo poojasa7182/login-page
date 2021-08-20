3129d851c3df1d4d5f3b2582051e563fb97553bb
poojasa7182 <76244864+poojasa7182@users.noreply.github.com>
2021-08-20 17:08:04 +0530

    Update conection.php

--- conection.php
+++ conection.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = mysqli_connect("localhost","root","@Adipoo2610","db");
+$mysqli = mysqli_connect("localhost","root","","db");
 if(mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     exit();
